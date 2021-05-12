8e607f0953d1160569809f8d51b72d669ee370bc
Cherepanov Artyom <cherepanov92@gmail.com>
2021-05-13 01:06:20 +0500

    Добавил примеры TS решений

---
+++ ts_exaples/Obj.ts
@@ -0,0 +1,47 @@
+//Объект, Interface
+
+const obj = {
+    name: 'Archy',
+    birthDay: 24,
+}
+
+
+const tsObj: { name:string, birthDay: number} = {
+    name: 'Archy',
+    birthDay: 24,
+}
+
+
+type objTypes = {
+    name: string,
+    birthDay: number | string,
+    male?: string
+}
+
+const tsObjWithType: objTypes = {
+    name: 'Archy',
+    birthDay: 24,
+}
+
+
+// Установка поля только для чтнеия
+interface objInterface {
+    readonly name: string;
+    birthDay: number | string;
+    male?: string;
+}
+
+const tsObjWithInterface: objTypes = {
+    name: 'Archy',
+    birthDay: 24,
+}
+
+// Сигнатура (для неизвестных структур данных)
+interface signatureInterface {
+    [n: string]: string|number;
+}
+
+const tsObjWithSignatureInterface: signatureInterface = {
+    name: 'Archy',
+    birthDay: '24',
+}
