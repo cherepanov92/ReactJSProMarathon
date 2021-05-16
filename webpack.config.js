c655f82367d9034db597a5d3c6c279e25ee75357
Cherepanov Artyom <cherepanov92@gmail.com>
2021-05-16 23:09:09 +0500

    lesson 3 добавил шрифт Karla в проект добавил обработчик file-loader в webpack

--- webpack.config.js
+++ webpack.config.js
@@ -54,18 +54,18 @@ module.exports = {
                 test: /\.svg$/,
                 use: ['@svgr/webpack', 'url-loader'],
             },
-            // {
-            //     test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-            //     use: [
-            //         {
-            //             loader: 'file-loader',
-            //             options: {
-            //                 name: '[name].[ext]',
-            //                 outputPath: 'fonts/',
-            //             },
-            //         },
-            //     ],
-            // },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[ext]',
+                            outputPath: 'fonts/',
+                        },
+                    },
+                ],
+            },
             // {
             //     test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/],
             //     use: ['url-loader'],
