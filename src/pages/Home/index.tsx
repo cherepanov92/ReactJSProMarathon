3d33d73d548c51eee936dda73e71e75fa20cd14c
Cherepanov Artyom <cherepanov92@gmail.com>
2021-05-16 23:02:56 +0500

    lesson 2 Вынес кнопку в компонент, написал первый propsInterface

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Header from "../../components/Header";
+import Layout from "../../components/Layout";
+import Button from "../../components/Button";
 
 import s from './Home.module.scss';
 
@@ -7,6 +9,15 @@ const HomePage = () => {
     return (
         <div className={s.root}>
             <Header />
+            <Layout>
+                <div>
+                    <h1>
+                        <b>Find</b> all you favorite <b>Pokemon</b>
+                    </h1>
+                    <p>text</p>
+                    <Button onClick={() => console.log('Button click')}>See pokemons</Button>
+                </div>
+            </Layout>
         </div>
     );
 };
