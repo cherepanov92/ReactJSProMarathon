c2439ac726be62e2e83846faf45a735adb8bacd8
Cherepanov Artyom <cherepanov92@gmail.com>
2021-05-20 02:11:20 +0500

    Реализовал отображение карточек покемонов

---
+++ src/components/PokemonCard/index.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import Heading from "../Heading";
+
+import s from './PokemonCard.module.scss';
+
+interface IStatus {
+    'hp': number;
+    'attack': number;
+    'defense': number;
+    'special-attack': number;
+    'special-defense': number;
+    'speed': number;
+}
+
+interface PokemonCardProps {
+    name: string;
+    stats: IStatus;
+    img: string;
+    types: Array<string>
+}
+
+const PokemonCard:React.FC<PokemonCardProps> = ({ name, stats, img, types }) => {
+    return (
+        <div className={s.root}>
+            <div className={s.infoWrap}>
+                <Heading type='h4' className={s.titleName}>
+                    {name}
+                </Heading>
+                <div className={s.statWrap}>
+                    <div className={s.statItem}>
+                        <div className={s.statValue}>
+                            {stats.attack}
+                        </div>
+                        Attack
+                    </div>
+                    <div className={s.statItem}>
+                        <div className={s.statValue}>
+                            {stats.defense}
+                        </div>
+                        Defense
+                    </div>
+                </div>
+                <div className={s.labelWrap}>
+                    {types.map(type => <span className={s.label}>{type}</span>)}
+                </div>
+            </div>
+            <div className={s.pictureWrap}>
+                <img src={img} alt={name} />
+            </div>
+        </div>
+    );
+};
+
+export default PokemonCard;
