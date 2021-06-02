a59c64cfb030ff74461f3ae8e2de6399c2d7a850
Cherepanov Artyom <cherepanov92@gmail.com>
2021-06-02 22:47:29 +0500

    Переписал процесс получения данных о конкретном покемоне

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,28 @@
 import Url from 'url';
 import getUriWithParamsConfig from "./getUriWithParamsConfig";
+
+interface IOptions {
+    method: string,
+    body?: string
+}
+
+interface IGetUriWithParamsConfig {
+    method: string,
+    uri: Partial<URL>,
+    body: object
+}
+
 async function req<T>(endpoint: string, query?: object): Promise<T> {
-    const uri = Url.format(getUriWithParamsConfig(endpoint, query))
-    return await fetch(uri).then((res) => res.json());
+    const {method, uri, body}: IGetUriWithParamsConfig = getUriWithParamsConfig(endpoint, query);
+
+    const options: IOptions = {
+        method
+    }
+
+    if (Object.keys(body).length > 0) {
+        options.body = JSON.stringify(body);
+    }
+    return await fetch(Url.format(uri), options).then((res) => res.json());
 }
 
 export default req;
